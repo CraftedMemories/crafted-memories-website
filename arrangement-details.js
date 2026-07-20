d519ef08c59c12143333cc36889f7351d50f6fd8
CraftedMemories <creatief-knutselen@outlook.com>
2026-07-20 21:12:34 +0200

    Create arrangement-details.js

---
+++ arrangement-details.js
@@ -0,0 +1,341 @@
+const arrangementDetails = {
+  "Little Love": [
+    "IMG_8039.jpeg",
+    "IMG_8040.jpeg",
+    "IMG_8041.jpeg",
+    "IMG_8042.jpeg",
+    "IMG_8043.jpeg",
+    "IMG_8044.jpeg"
+  ],
+
+  "Golden Spirit": [
+    "IMG_7971.jpeg",
+    "IMG_7972.jpeg",
+    "IMG_7973.jpeg",
+    "IMG_7974.jpeg",
+    "IMG_7975.jpeg"
+  ],
+
+  "Tropical Toucan": [
+    "IMG_6877.jpeg",
+    "IMG_6878.jpeg",
+    "IMG_6879.jpeg"
+  ],
+
+  "Wild Jaguar": [
+    "IMG_6416.jpeg",
+    "IMG_6417.jpeg",
+    "IMG_6418.jpeg"
+  ],
+
+  "Love on the Road": [
+    "IMG_6408.jpeg",
+    "IMG_6409.jpeg",
+    "IMG_6410.jpeg",
+    "IMG_6411.jpeg",
+    "IMG_6412.jpeg"
+  ],
+
+  "Bohemian Spirit": [
+    "IMG_6404.jpeg",
+    "IMG_6405.jpeg",
+    "IMG_6406.jpeg"
+  ],
+
+  "Love in Bloom": [
+    "IMG_6396.jpeg",
+    "IMG_6397.jpeg",
+    "IMG_6398.jpeg",
+    "IMG_6399.jpeg",
+    "IMG_6400.jpeg"
+  ],
+
+  "Golden Honey": [
+    "IMG_6383.jpeg",
+    "IMG_6384.jpeg",
+    "IMG_6385.jpeg"
+  ],
+
+  "Blue Blossom": [
+    "IMG_6378.jpeg",
+    "IMG_6379.jpeg",
+    "IMG_6380.jpeg",
+    "IMG_6381.jpeg"
+  ],
+
+  "Pirate Adventure": [
+    "IMG_6371.jpeg",
+    "IMG_6373.jpeg",
+    "IMG_6374.jpeg"
+  ],
+
+  "Happy Easter": [
+    "IMG_6359.jpeg",
+    "IMG_6360.jpeg",
+    "IMG_6361.jpeg",
+    "IMG_6362.jpeg"
+  ],
+
+  "Tropical Monkey": [
+    "IMG_6338.jpeg",
+    "IMG_6339.jpeg",
+    "IMG_6340.jpeg",
+    "IMG_6341.jpeg"
+  ],
+
+  "Spring Bunnies": [
+    "IMG_6330.jpeg",
+    "IMG_6331.jpeg",
+    "IMG_6332.jpeg",
+    "IMG_6333.jpeg"
+  ],
+
+  "Midnight Lilies Duo": [
+    "IMG_6324.jpeg",
+    "IMG_6325.jpeg",
+    "IMG_6326.jpeg",
+    "IMG_6327.jpeg"
+  ],
+
+  "Spirit of the Wolf": [
+    "IMG_6318.jpeg",
+    "IMG_6319.jpeg",
+    "IMG_6320.jpeg"
+  ],
+
+  "Lemonade Sunshine": [
+    "IMG_6314.jpeg",
+    "IMG_6315.jpeg",
+    "IMG_6316.jpeg",
+    "IMG_6317.jpeg"
+  ],
+
+  "Rosy Bunny": [
+    "IMG_6289.jpeg",
+    "IMG_6290.jpeg",
+    "IMG_6291.jpeg",
+    "IMG_6292.jpeg"
+  ],
+
+  "Happy Hedgehog": [
+    "IMG_6281.jpeg",
+    "IMG_6282.jpeg",
+    "IMG_6283.jpeg",
+    "IMG_6284.jpeg"
+  ],
+
+  "Golden Blue": [
+    "IMG_6274.jpeg",
+    "IMG_6275.jpeg",
+    "IMG_6276.jpeg",
+    "IMG_6277.jpeg"
+  ],
+
+  "Pink Bird": [
+    "IMG_6257.jpeg",
+    "IMG_6258.jpeg",
+    "IMG_6259.jpeg"
+  ],
+
+  "Macaron": [
+    "IMG_6252.jpeg",
+    "IMG_6253.jpeg",
+    "IMG_6254.jpeg",
+    "IMG_6255.jpeg"
+  ],
+
+  "Amy": [
+    "IMG_6245.jpeg",
+    "IMG_6246.jpeg",
+    "IMG_6247.jpeg",
+    "IMG_6248.jpeg"
+  ],
+
+  "Blue Lion": [
+    "IMG_6210.jpeg",
+    "IMG_6211.jpeg",
+    "IMG_6212.jpeg"
+  ],
+
+  "Golden Lady": [
+    "IMG_6204.jpeg",
+    "IMG_6205.jpeg",
+    "IMG_6206.jpeg",
+    "IMG_6207.jpeg"
+  ],
+
+  "Pink Queen": [
+    "IMG_6199.jpeg",
+    "IMG_6200.jpeg",
+    "IMG_6201.jpeg",
+    "IMG_6202.jpeg"
+  ],
+
+  "Spring Bunny": [
+    "IMG_6193.jpeg",
+    "IMG_6194.jpeg",
+    "IMG_6195.jpeg"
+  ],
+
+  "Sweet Donuts": [
+    "IMG_6179.jpeg",
+    "IMG_6180.jpeg",
+    "IMG_6181.jpeg",
+    "IMG_6182.jpeg"
+  ],
+
+  "Peaky Blinders": [
+    "IMG_6172.jpeg",
+    "IMG_6173.jpeg",
+    "IMG_6174.jpeg",
+    "IMG_6175.jpeg",
+    "IMG_6176.jpeg"
+  ],
+
+  "Herfst Hooglander": [
+    "IMG_6168.jpeg",
+    "IMG_6169.jpeg",
+    "IMG_6170.jpeg"
+  ],
+
+  "Vrolijke Giraffe": [
+    "IMG_6164.jpeg",
+    "IMG_6163.jpeg",
+    "IMG_6165.jpeg"
+  ],
+
+  "Kleurrijke Leeuw": [
+    "IMG_6145.jpeg",
+    "IMG_6146.jpeg",
+    "IMG_6147.jpeg",
+    "IMG_6148.jpeg"
+  ],
+
+  "Blauw Porselein": [
+    "IMG_6140.jpeg",
+    "IMG_6141.jpeg",
+    "IMG_6142.jpeg"
+  ],
+
+  "Tropische Papegaai": [
+    "IMG_6106.jpeg",
+    "IMG_6107.jpeg",
+    "IMG_6108.jpeg",
+    "IMG_6109.jpeg",
+    "IMG_6110.jpeg"
+  ],
+
+  "Candy Girl": [
+    "IMG_6099.jpeg",
+    "IMG_6100.jpeg",
+    "IMG_6101.jpeg",
+    "IMG_6102.jpeg"
+  ],
+
+  "Roze Leliedroom": [
+    "IMG_6079.jpeg",
+    "IMG_6080.jpeg",
+    "IMG_6081.jpeg",
+    "IMG_6082.jpeg"
+  ],
+
+  "Wild Love": [
+    "IMG_6074.jpeg",
+    "IMG_6075.jpeg",
+    "IMG_6076.jpeg",
+    "IMG_6077.jpeg"
+  ],
+
+  "Festive Red": [
+    "IMG_6345.jpeg",
+    "IMG_6346.jpeg",
+    "IMG_6343.jpeg",
+    "IMG_6344.jpeg"
+  ],
+
+  "Hello Fall": [
+    "IMG_6390.jpeg",
+    "IMG_6391.jpeg",
+    "IMG_6392.jpeg",
+    "IMG_6393.jpeg",
+    "IMG_6394.jpeg"
+  ],
+
+  "Happy Halloween": [
+    "IMG_6119.jpeg",
+    "IMG_6120.jpeg",
+    "IMG_6121.jpeg",
+    "IMG_6122.jpeg"
+  ],
+
+  "Pink Leopard": [
+    "IMG_0102.jpeg",
+    "IMG_0103.jpeg",
+    "IMG_0104.jpeg",
+    "IMG_0105.jpeg"
+  ],
+
+  "Golden Giraffe": [
+    "IMG_0082.jpeg",
+    "IMG_0083.jpeg",
+    "IMG_0084.jpeg",
+    "IMG_0085.jpeg"
+  ],
+
+  "Blue Cow": [
+    "IMG_0094.jpeg",
+    "IMG_0095.jpeg",
+    "IMG_0096.jpeg",
+    "IMG_0097.jpeg"
+  ],
+
+  "Golden Tiger": [
+    "IMG_0086.jpeg",
+    "IMG_0087.jpeg",
+    "IMG_0088.jpeg",
+    "IMG_0089.jpeg"
+  ],
+
+  "Black Panther": [
+    "IMG_0090.jpeg",
+    "IMG_0091.jpeg",
+    "IMG_0092.jpeg",
+    "IMG_0093.jpeg"
+  ],
+
+  "Pink Peacock": [
+    "Peacock1.jpeg",
+    "Peacock2.jpeg",
+    "Peacock3.jpeg",
+    "Peacock4.jpeg"
+  ],
+
+  "Blue Peacock": [
+    "IMG_0333.jpeg",
+    "IMG_0334.jpeg",
+    "IMG_0335.jpeg",
+    "IMG_0336.jpeg"
+  ],
+
+  "Mint Peacock": [
+    "IMG_0415.jpeg",
+    "IMG_0416.jpeg",
+    "IMG_0417.jpeg",
+    "IMG_0418.jpeg"
+  ],
+
+  "Mediterranean Summer": [
+    "IMG_0985.jpeg",
+    "IMG_0986.jpeg",
+    "IMG_0987.jpeg",
+    "IMG_0988.jpeg",
+    "IMG_0989.jpeg"
+  ],
+
+  "Mediterranean Joy": [
+    "IMG_9876.jpeg",
+    "IMG_9877.jpeg",
+    "IMG_9878.jpeg",
+    "IMG_9879.jpeg"
+  ]
+};
