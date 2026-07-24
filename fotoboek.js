3f57114114c3c21700474e7cbee25979e80e2515
CraftedMemories <creatief-knutselen@outlook.com>
2026-07-24 22:16:44 +0200

    Update fotoboek.js

--- fotoboek.js
+++ fotoboek.js
@@ -4,459 +4,458 @@ const arrangements = [
     image: "4442E87A-0247-4E8A-8902-3BAC61C6C6DF.jpeg",
     status: "available",
     price: "€30,00",
-        size: "23 cm hoog × 20 cm Ø"
+        size: "23 cm hoog × Ø 20 cm"
   },
    {
     name: "Mediterranean Joy",
     image: "710E94DE-8F59-44D9-BA0C-98CC2E577D07.jpeg",
     status: "available",
-    price: "€25,00"
-       size: "22 cm hoog × 25 cm Ø"
+    price: "€25,00",
+       size: "22 cm hoog × Ø 25 cm"
   },
      {
     name: "Little Love",
     image: "D4677581-2B73-4B9B-A0F0-60625F8064BF.jpeg",
     status: "available",
-    price: "€40,00"
-         size: "30 cm hoog × 23 cm Ø"
+    price: "€40,00",
+         size: "30 cm hoog × Ø 23 cm"
   },
       {
     name: "Pink Peacock",
     image: "FA12FDCD-B7FC-48DC-8ADE-177BA1FF5298.jpeg",
     status: "available",
-    price: "€45,00"
-          size: "40 cm hoog × 24 cm Ø"
+    price: "€45,00",
+          size: "40 cm hoog × Ø 24 cm"
   },
   {
     name: "Blue Peacock",
     image: "EF7F3280-6847-4DA5-9C60-107EE7EDFE64.jpeg",
     status: "available",
-    price: "€40,00"
-      size: "34 cm hoog × 21 cm Ø"
+    price: "€40,00",
+      size: "34 cm hoog × Ø 21 cm"
   },
   {
     name: "Mint Peacock",
     image: "662C84C8-D5E4-4F25-B096-53558DE90D50.jpeg",
     status: "available",
-    price: "€40,00"
-      size: "33 cm hoog × 25 cm Ø"
+    price: "€40,00",
+      size: "33 cm hoog × Ø 25 cm"
   },
      {
     name: "Pink Leopard",
     image: "DC096568-50DB-41FB-8ECD-50F2D067AAD6.jpeg",
     status: "available",
-    price: "€17,50"
-         size: "15 cm hoog × 18 cm Ø"
+    price: "€17,50",
+         size: "15 cm hoog × Ø 18 cm"
   },
   {
     name: "Golden Giraffe",
     image: "29B1C3DC-DB31-483E-B02D-5E54059016B4.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "13 cm hoog × 14 cm Ø"
+    price: "€15,00",
+      size: "13 cm hoog × Ø 14 cm"
   },
   {
     name: "Blue Cow",
     image: "036F5C42-7427-4302-9F9C-46C3FCF0E206.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "13 cm hoog × 14 cm Ø"
+    price: "€15,00",
+      size: "13 cm hoog × Ø 14 cm"
   },
   {
     name: "Golden Tiger",
     image: "848FF973-BFE2-4382-BDB3-947B43DB2B66.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "16 cm hoog × 13 cm Ø"
+    price: "€17,50",
+      size: "16 cm hoog × Ø 13 cm"
   },
   {
     name: "Black Panther",
     image: "4ED008FE-8D99-4BB8-A022-7192D95593D0.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "17 cm hoog × 11 cm Ø"
+    price: "€15,00",
+      size: "17 cm hoog × Ø 11 cm"
   },
   {
     name: "Roze Engeldroom",
     image: "57F40836-972B-483B-82A8-AAA1FFB4AE77.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "21 cm hoog × 16 cm Ø"
+    price: "€17,50",
+      size: "21 cm hoog × Ø 16 cm"
   },
   {
     name: "Golden Spirit",
     image: "D11025B7-1912-438D-A3B9-B5832A6A0555.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "20 cm hoog × 20 cm Ø"
+    price: "€15,00",
+      size: "20 cm hoog × Ø 20 cm"
   },
   {
     name: "Tropical Toucan",
     image: "854B5BD3-8966-437F-9D71-12C3DACFB6FB.jpeg",
     status: "available",
-    price: "€22,50"
-      size: "29 cm hoog × 15 cm Ø"
+    price: "€22,50",
+      size: "29 cm hoog × Ø 15 cm"
   },
   {
     name: "Wild Jaguar",
     image: "8468882A-527C-44DA-A0C0-D5A6017481DC.jpeg",
     status: "available",
-    price: "€25,00"
-      size: "26 cm hoog × 19 cm Ø"
+    price: "€25,00",
+      size: "26 cm hoog × Ø 19 cm"
   },
   {
     name: "Love on the Road",
     image: "A57DE690-F9BF-4D19-A2E3-748DB9484E7C.jpeg",
     status: "available",
-    price: "€22,50"
-      size: "25 cm hoog × 14 cm Ø"
+    price: "€22,50",
+      size: "25 cm hoog × Ø 14 cm"
   },
   {
     name: "Bohemian Spirit",
     image: "090451CC-C543-40FC-B6D9-E20D0829C00D.jpeg",
     status: "available",
-    price: "€22,50"
-      size: "25 cm hoog × 18 cm Ø"
+    price: "€22,50",
+      size: "25 cm hoog × Ø 18 cm"
   },
   {
     name: "Love in Bloom",
     image: "D194EE28-2A0C-44E4-8BAC-514EE96E4E8F.jpeg",
     status: "available",
-    price: "€22,50"
-      size: "25 cm hoog × 14 cm Ø"
+    price: "€22,50",
+      size: "25 cm hoog × Ø 14 cm"
   },
   {
     name: "Golden Honey",
     image: "CC9B7A34-7C44-48B6-AF4B-1F407263963D.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "17 cm hoog × 12 cm Ø"
+    price: "€15,00",
+      size: "17 cm hoog × Ø 12 cm"
   },
   {
     name: "Blue Blossom",
     image: "460CA9E7-F4CF-480D-95D3-08B730E9B6D0.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "20 cm hoog × 13 cm Ø"
+    price: "€17,50",
+      size: "20 cm hoog × Ø 13 cm"
   },
   {
     name: "Pirate Adventure",
     image: "F5DE1B63-8C52-4116-82D5-3D9F41937333.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "19 cm hoog × 14 cm Ø"
+    price: "€15,00",
+      size: "19 cm hoog × Ø 14 cm"
   },
   {
     name: "Happy Easter",
     image: "3C48F941-2E16-4BCC-8D1F-EB5649BBA481.jpeg",
     status: "available",
-    price: "€20,00"
-      size: "22 cm hoog × 19 cm Ø"
+    price: "€20,00",
+      size: "22 cm hoog × Ø 19 cm"
   },
   {
     name: "Tropical Monkey",
     image: "265484DB-690C-4C05-BFA6-F2ED825C914F.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "15 cm hoog × 15 cm Ø"
+    price: "€15,00",
+      size: "15 cm hoog × Ø 15 cm"
   },
   {
     name: "Spring Bunnies",
     image: "75D3AB82-E637-4FCC-BC44-819FCD14A202.jpeg",
     status: "available",
-    price: "€22,50"
-      size: "30 cm hoog × 16 cm Ø"
+    price: "€22,50",
+      size: "30 cm hoog × Ø 16 cm"
   },
   {
     name: "Midnight Lilies Duo",
     image: "53279506-7FA6-405E-B83E-EFF5D2EBFD4B.jpeg",
     status: "available",
-    price: "€32,50"
-      size: "21 cm hoog × 15 cm Ø"
-      size: "17 cm hoog × 13 cm Ø"
+    price: "€32,50",
+      size: "Set: 21 cm hoog × Ø 15 cm | 17 cm hoog × Ø 13 cm"
   },
   {
     name: "Spirit of the Wolf",
     image: "B7987179-F559-4751-9D40-14601573AF6A.jpeg",
     status: "available",
-    price: "€25,00"
-      size: "25 cm hoog × 18 cm Ø"
+    price: "€25,00",
+      size: "25 cm hoog × Ø 18 cm"
   },
   {
     name: "Lemonade Sunshine",
     image: "859BB12A-E05E-459A-8221-6F5D8F6C60BA.jpeg",
     status: "available",
-    price: "€22,50"
-      size: "25 cm hoog × 15 cm Ø"
+    price: "€22,50",
+      size: "25 cm hoog × Ø 15 cm"
   },
   {
     name: "Rosy Bunny",
     image: "11561348-AF0D-45D8-99F7-B15BEDB73F09.jpeg",
     status: "available",
-    price: "€12,50"
-      size: "10 cm hoog × 14 cm Ø"
+    price: "€12,50",
+      size: "10 cm hoog × Ø 14 cm"
   },
   {
     name: "Happy Hedgehog",
     image: "A346A8FA-4099-4D6C-8237-F7E12E0F0020.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "18 cm hoog × 18 cm Ø"
+    price: "€17,50",
+      size: "18 cm hoog × Ø 18 cm"
   },
   {
     name: "Golden Blue",
     image: "C2219FE5-AB53-43A4-9CB3-22483AA678FB.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "17 cm hoog × 12 cm Ø"
+    price: "€15,00",
+      size: "17 cm hoog × Ø 12 cm"
   },
   {
     name: "Pink Bird",
     image: "FCF7359E-06F6-4423-BA02-F7BF132F2C1D.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "23 cm hoog × 16 cm Ø"
+    price: "€17,50",
+      size: "23 cm hoog × Ø 16 cm"
   },
   {
     name: "Macaron",
     image: "D2C47E77-7B05-4AF8-A9F4-7E8927A22E82.jpeg",
     status: "available",
-    price: "€20,00"
-      size: "20 cm hoog × 15 cm Ø"
+    price: "€20,00",
+      size: "20 cm hoog × Ø 15 cm"
   },
   {
     name: "Amy",
     image: "A383C0F9-3424-445E-A973-BFA6EA468EBE.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "22 cm hoog × 15 cm Ø"
+    price: "€17,50",
+      size: "22 cm hoog × Ø 15 cm"
   },
   {
     name: "Blue Lion",
     image: "84FCDBD3-311E-4E44-981B-78764BBC6776.jpeg",
     status: "available",
-    price: "€30,00"
-      size: "29 cm hoog × 16 cm Ø"
+    price: "€30,00",
+      size: "29 cm hoog × Ø 16 cm"
   },
   {
     name: "Golden Lady",
     image: "B9BEB524-2005-4CE4-9A93-96E87178DC9C.jpeg",
     status: "available",
-    price: "€30,00"
-      size: "30 cm hoog × 18 cm Ø"
+    price: "€30,00",
+      size: "30 cm hoog × Ø 18 cm"
   },
   {
     name: "Pink Queen",
     image: "0D955B3A-6813-4FD6-8666-0E5C2A20556A.jpeg",
     status: "available",
-    price: "€30,00"
-      size: "29 cm hoog × 216cm Ø"
+    price: "€30,00",
+      size: "29 cm hoog × Ø 16 cm"
   },
   {
     name: "Spring Bunny",
     image: "13B71E32-4823-4034-B03C-8C5F62D5A438.jpeg",
     status: "available",
-    price: "€22,50"
-      size: "26 cm hoog × 14 cm Ø"
+    price: "€22,50",
+      size: "26 cm hoog × Ø 14 cm"
   },
   {
     name: "Sweet Donuts",
     image: "66C74DAD-9F39-491D-A794-E37A1E078764.jpeg",
     status: "available",
-    price: "€20,00"
-      size: "19 cm hoog × 15 cm Ø"
+    price: "€20,00",
+      size: "19 cm hoog × Ø 15 cm"
   },
   {
     name: "Peaky Blinders",
     image: "4CC73CB2-A297-49A9-A15D-7CFB54C53876.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "21 cm hoog × 14 cm Ø"
+    price: "€17,50",
+      size: "21 cm hoog × Ø 14 cm"
   },
   {
     name: "Herfst Hooglander",
     image: "1C4B6815-5A5D-46C5-BF3D-37EE96FB6384.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "22 cm hoog × 18 cm Ø"
+    price: "€17,50",
+      size: "22 cm hoog × Ø 18 cm"
   },
   {
     name: "Vrolijke Giraffe",
     image: "5423B09F-166A-4CC8-8433-E43307641A10.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "21 cm hoog × 18 cm Ø"
+    price: "€17,50",
+      size: "21 cm hoog × Ø 18 cm"
   },
   {
     name: "Kleurrijke Leeuw",
     image: "8A71037D-7E5D-4849-B682-B5F0BE226D37.jpeg",
     status: "available",
-    price: "€27,50"
-      size: "31 cm hoog × 18 cm Ø"
+    price: "€27,50",
+      size: "31 cm hoog × Ø 18 cm"
   },
   {
     name: "Blauw Porselein",
     image: "80973A8B-E8C1-4E6E-8BE6-E89271D095E2.jpeg",
     status: "available",
-    price: "€27,50"
-      size: "31 cm hoog × 24 cm Ø"
+    price: "€27,50",
+      size: "31 cm hoog × Ø 24 cm"
   },
   {
     name: "Tropische Papegaai",
     image: "4AC7D2DF-E16E-4489-9359-DF0E3EA207FD.jpeg",
     status: "available",
-    price: "€20,00"
-      size: "20 cm hoog × 19 cm Ø"
+    price: "€20,00",
+      size: "20 cm hoog × Ø 19 cm"
   },
   {
     name: "Candy Girl",
     image: "E285DA9A-2162-478E-966E-C53BE5B30A87.jpeg",
     status: "available",
-    price: "€17,50"
-      size: "21 cm hoog × 19 cm Ø"
+    price: "€17,50",
+      size: "21 cm hoog × Ø 19 cm"
   },
   {
     name: "Roze Leliedroom",
     image: "EF252CB8-1FD1-49EE-9B43-39793FCE65DA.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "20 cm hoog × 20 cm Ø"
+    price: "€15,00",
+      size: "20 cm hoog × Ø 20 cm"
   },
   {
     name: "Wild Love",
     image: "6D89D0C3-C4ED-4675-84BA-D15532F8EE0F.jpeg",
     status: "available",
-    price: "€15,00"
-      size: "15 cm hoog × 15 cm Ø"
+    price: "€15,00",
+      size: "15 cm hoog × Ø 15 cm"
   },
 
   {
     name: "Hello Fall",
     image: "33C679C4-E7E1-408C-B704-3903C616CDB2.jpeg",
     status: "available",
-    price: "€27,50"
-      size: "27 cm hoog × 19 cm Ø"
+    price: "€27,50",
+      size: "27 cm hoog × Ø 19 cm"
   },
   {
     name: "Happy Halloween",
     image: "A4D9BF8F-1CA8-4F8B-8D11-59C34707BBAC.jpeg",
     status: "available",
-    price: "€20,00"
-      size: "23 cm hoog × 19 cm Ø"
+    price: "€20,00",
+      size: "23 cm hoog × Ø 19 cm"
   },
   {
   name: "Winter Magic",
   image: "58DCDA64-042F-4ABE-B4F7-5D0205488E88.jpeg",
-  price: "€20,00"
-      size: "21 cm hoog × 19 cm Ø"
+  price: "€20,00",
+      size: "21 cm hoog × Ø 19 cm"
 },
 {
   name: "Gingerbread Christmas",
   image: "C543CA1A-D6FC-4E7D-A5B2-D0F548281013.jpeg",
-  price: "€20,00"
-    size: "21 cm hoog × 17 cm Ø"
+  price: "€20,00",
+    size: "21 cm hoog × Ø 17 cm"
 },
 {
   name: "Grinchmas",
   image: "97DDDB79-01A9-492F-8502-FF01D65AE6CE.jpeg",
-  price: "€17,50"
-    size: "21 cm hoog × 15 cm Ø"
+  price: "€17,50",
+    size: "21 cm hoog × Ø 15 cm"
 },
 {
   name: "Winter Blossom",
   image: "10433C12-7BF6-43DB-9AE0-11981ED219F3.jpeg",
-  price: "€15,00"
-    size: "18 cm hoog × 18 cm Ø"
+  price: "€15,00",
+    size: "18 cm hoog × Ø 18 cm"
 },
 {
   name: "Santa Sparkle Express",
   image: "B045F2E7-1BBE-411B-89F3-C1B6EFE4E4F8.jpeg",
-  price: "€32,50"
-    size: "33 cm hoog × 18 cm Ø"
+  price: "€32,50",
+    size: "33 cm hoog × Ø 18 cm"
 },
 {
   name: "Pink Winter Wishes",
   image: "0E82EF08-6CF4-4591-8891-020D77AC53BB.jpeg",
-  price: "€15,00"
-    size: "15 cm hoog × 15 cm Ø"
+  price: "€15,00",
+    size: "15 cm hoog × Ø 15 cm"
 },
 {
   name: "Christmas Forest Friends",
   image: "B9532678-160B-4671-BFC4-ED15B3D7B489.jpeg",
-  price: "€15,00"
-    size: "15 cm hoog × 15 cm Ø"
+  price: "€15,00",
+    size: "15 cm hoog × Ø 15 cm"
 },
 {
   name: "Pink Christmas Butterfly",
   image: "356044CF-8097-49A2-87F2-EAC2B0F78C97.jpeg",
-  price: "€20,00"
-    size: "21 cm hoog × 20 cm Ø"
+  price: "€20,00",
+    size: "21 cm hoog × Ø 20 cm"
 },
 {
   name: "Christmas Reindeer & Penguin",
   image: "B37C12A0-F07E-4865-8126-04968A61E282.jpeg",
-  price: "€15,00"
-    size: "15 cm hoog × 15 cm Ø"
+  price: "€15,00",
+    size: "15 cm hoog × Ø 15 cm"
 },
 {
   name: "Merry Christmas Santa",
   image: "39FC157E-DBBE-42F5-BFDD-981FDFE73C64.jpeg",
-  price: "€22,50"
-    size: "25 cm hoog × 15 cm Ø"
+  price: "€22,50",
+    size: "25 cm hoog × Ø 15 cm"
 },
     {
   name: "Love on Tour",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€40,00"
-    size: "30 cm hoog × 19 cm Ø"
+  price: "€40,00",
+    size: "30 cm hoog × Ø 19 cm"
 },
 {
   name: "Tropical Blue",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€17,50"
-    size: "21 cm hoog × 14 cm Ø"
+  price: "€17,50",
+    size: "21 cm hoog × Ø 14 cm"
 },
 {
   name: "Honey Bee",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€15,00"
-    size: "16 cm hoog × 17 cm Ø"
+  price: "€15,00",
+    size: "16 cm hoog × Ø 17 cm"
 },
 {
   name: "Minty Garden",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€15,00"
-    size: "19 cm hoog × 15 cm Ø"
+  price: "€15,00",
+    size: "19 cm hoog × Ø 15 cm"
 },
 {
   name: "Golden Magic",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€22,50"
-    size: "25 cm hoog × 13 cm Ø"
+  price: "€22,50",
+    size: "25 cm hoog × Ø 13 cm"
 },
 {
   name: "Blue Christmas Magic",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€20,00"
-    size: "23 cm hoog × 17 cm Ø"
+  price: "€20,00",
+    size: "23 cm hoog × Ø 17 cm"
 },
 {
   name: "Pink Christmas Mischief",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€25,00"
-    size: "26 cm hoog × 22 cm Ø"
+  price: "€25,00",
+    size: "26 cm hoog × Ø 22 cm"
 },
 {
   name: "Christmas Together",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€15,00"
-    size: "15 cm hoog × 15 cm Ø"
+  price: "€15,00",
+    size: "15 cm hoog × Ø 15 cm"
 },
 {
   name: "Winter Sisters",
   image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-  price: "€25,00"
-    size: "26 cm hoog × 19 cm Ø"
+  price: "€25,00",
+    size: "26 cm hoog × Ø 19 cm"
 },
 
   {
