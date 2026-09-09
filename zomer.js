d35ed01dc75ca4251318258c551fa13899017879
CraftedMemories <creatief-knutselen@outlook.com>
2026-09-09 19:10:00 +0200

    Update zomer.js

--- zomer.js
+++ zomer.js
@@ -47,3 +47,24 @@ zomerVazen.forEach((vaas) => {
 
   beschikbaarGrid.appendChild(kaart);
 });
+const zomerVerkocht = [
+  {
+    name: "Tropical Paradise",
+    image: "9FA66EC2-C61C-44E9-97C7-033A707742AD.jpeg"
+  }
+];
+
+const verkochtGrid = document.getElementById("zomer-verkocht");
+
+zomerVerkocht.forEach((vaas) => {
+  const kaart = document.createElement("div");
+  kaart.className = "fotoboek-item";
+
+  kaart.innerHTML = `
+    <img src="${vaas.image}" alt="${vaas.name} vaas">
+    <h2>${vaas.name}</h2>
+    <p>SOLD OUT</p>
+  `;
+
+  verkochtGrid.appendChild(kaart);
+});
