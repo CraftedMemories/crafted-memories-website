b2f61e0566ccb8b34833ee80e46517d584ea3bee
CraftedMemories <creatief-knutselen@outlook.com>
2026-09-09 19:39:24 +0200

    Update zomer.js

--- zomer.js
+++ zomer.js
@@ -4,31 +4,6 @@ const zomerVazen = [
     image: "4442E87A-0247-4E8A-8902-3BAC61C6C6DF.jpeg",
     price: "€30,00"
   },
-  {
-    name: "Mediterranean Joy",
-    image: "710E94DE-8F59-44D9-BA0C-98CC2E577D07.jpeg",
-    price: "€25,00"
-  },
-  {
-    name: "Tropical Toucan",
-    image: "854B5BD3-8966-437F-9D71-12C3DACFB6FB.jpeg",
-    price: "€22,50"
-  },
-  {
-    name: "Tropical Monkey",
-    image: "265484DB-690C-4C05-BFA6-F2ED825C914F.jpeg",
-    price: "€15,00"
-  },
-  {
-    name: "Tropische Papegaai",
-    image: "4AC7D2DF-E16E-4489-9359-DF0E3EA207FD.jpeg",
-    price: "€20,00"
-  },
-  {
-    name: "Tropical Blue",
-    image: "85C51A86-FC00-4763-B7ED-3511754EF2CD.png",
-    price: "€17,50"
-  }
 ];
 
 const beschikbaarGrid = document.getElementById("zomer-beschikbaar");
