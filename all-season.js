5b78272fd0731705ca6a20eed1f5f81d6c08c4cc
CraftedMemories <creatief-knutselen@outlook.com>
2026-09-11 19:32:37 +0200

    Update all-season.js

--- all-season.js
+++ all-season.js
@@ -1,12 +1,305 @@
 const allSeasonVazen = [
   {
-    name: "Mafia Royale",
-    image: "31FB12F1-5037-4913-88D0-F16992F3C587.png",
+    name: "Rose Garden Elegance",
+    image: "9B3B61E2-AC61-4CDB-8334-1EA002A933FD.png",
+    price: "€50,00"
+  },
+  {
+    name: "Little Love",
+    image: "D4677581-2B73-4B9B-A0F0-60625F8064BF.jpeg",
+    price: "€40,00"
+  },
+  {
+    name: "Pink Peacock",
+    image: "FA12FDCD-B7FC-48DC-8ADE-177BA1FF5298.jpeg",
+    price: "€45,00"
+  },
+  {
+    name: "Blue Peacock",
+    image: "EF7F3280-6847-4DA5-9C60-107EE7EDFE64.jpeg",
+    price: "€40,00"
+  },
+  {
+    name: "Mint Peacock",
+    image: "662C84C8-D5E4-4F25-B096-53558DE90D50.jpeg",
+    price: "€40,00"
+  },
+  {
+    name: "Pink Leopard",
+    image: "DC096568-50DB-41FB-8ECD-50F2D067AAD6.jpeg",
+    price: "€17,50"
+  },
+  {
+    name: "Golden Giraffe",
+    image: "29B1C3DC-DB31-483E-B02D-5E54059016B4.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Blue Cow",
+    image: "036F5C42-7427-4302-9F9C-46C3FCF0E206.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Golden Tiger",
+    image: "848FF973-BFE2-4382-BDB3-947B43DB2B66.jpeg",
+    price: "€17,50"
+  },
+  {
+    name: "Black Panther",
+    image: "4ED008FE-8D99-4BB8-A022-7192D95593D0.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Roze Engeldroom",
+    image: "57F40836-972B-483B-82A8-AAA1FFB4AE77.jpeg",
+    price: "€17,50"
+  },
+  {
+    name: "Golden Spirit",
+    image: "D11025B7-1912-438D-A3B9-B5832A6A0555.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Tropical Toucan",
+    image: "854B5BD3-8966-437F-9D71-12C3DACFB6FB.jpeg",
+    price: "€22,50"
+  },
+  {
+    name: "Wild Jaguar",
+    image: "8468882A-527C-44DA-A0C0-D5A6017481DC.jpeg",
+    price: "€25,00"
+  },
+  {
+    name: "Bohemian Spirit",
+    image: "090451CC-C543-40FC-B6D9-E20D0829C00D.jpeg",
+    price: "€22,50"
+  },
+  {
+    name: "Golden Honey",
+    image: "CC9B7A34-7C44-48B6-AF4B-1F407263963D.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Blue Blossom",
+    image: "460CA9E7-F4CF-480D-95D3-08B730E9B6D0.jpeg",
+    price: "€17,50"
+  },
+  {
+    name: "Pirate Adventure",
+    image: "F5DE1B63-8C52-4116-82D5-3D9F41937333.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Tropical Monkey",
+    image: "265484DB-690C-4C05-BFA6-F2ED825C914F.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Midnight Lilies Duo",
+    image: "53279506-7FA6-405E-B83E-EFF5D2EBFD4B.jpeg",
+    price: "€32,50"
+  },
+  {
+    name: "Spirit of the Wolf",
+    image: "B7987179-F559-4751-9D40-14601573AF6A.jpeg",
+    price: "€25,00"
+  },
+  {
+    name: "Golden Blue",
+    image: "C2219FE5-AB53-43A4-9CB3-22483AA678FB.jpeg",
+    price: "€15,00"
+  },
+  {
+    name: "Pink Bird",
+    image: "FCF7359E-06F6-4423-BA02-F7BF132F2C1D.jpeg",
+    price: "€17,50"
+  },
+  {
+    name: "Macaron",
+    image: "D2C47E77-7B05-4AF8-A9F4-7E8927A22E82.jpeg",
+    price: "€20,00"
+  },
+  {
+    name: "Blue Lion",
+    image: "84FCDBD3-311E-4E44-981B-78764BBC6776.jpeg",
+    price: "€30,00"
+  },
+  {
+    name: "Golden Lady",
+    image: "B9BEB524-2005-4CE4-9A93-96E87178DC9C.jpeg",
+    price: "€30,00"
+  },
+  {
+    name: "Pink Queen",
+    image: "0D955B3A-6813-4FD6-8666-0E5C2A20556A.jpeg",
+    price: "€30,00"
+  },
+  {
+    name: "Sweet Donuts",
+    image: "66C74DAD-9F39-491D-A794-E37A1E078764.jpeg",
+    price: "€20,00"
+  },
+  {
+    name: "Vrolijke Giraffe",
+    image: "5423B09F-166A-4CC8-8433-E43307641A10.jpeg",
+    price: "€17,50"
+  },
+  {
+    name: "Kleurrijke Leeuw",
+    image: "8A71037D-7E5D-4849-B682-B5F0BE226D37.jpeg",
     price: "€27,50"
+  },
+  {
+    name: "Blauw Porselein",
+    image: "80973A8B-E8C1-4E6E-8BE6-E89271D095E2.jpeg",
+    price: "€27,50"
+  },
+  {
+    name: "Tropische Papegaai",
+    image: "4AC7D2DF-E16E-4489-9359-DF0E3EA207FD.jpeg",
+    price: "€20,00"
+  },
+  {
+    name: "Candy Girl",
+    image: "E285DA9A-2162-478E-966E-C53BE5B30A87.jpeg",
+    price: "€17,50"
+  },
+  {
+    name: "Roze Leliedroom",
+    image: "EF252CB8-1FD1-49EE-9B43-39793FCE65DA.jpeg",
+    price: "€15,00"
+  }
+];
+
+const allSeasonVerkocht = [
+  {
+    name: "Bohemian Bloom",
+    image: "83E65058-13F1-4FFC-A1A5-9F2539CB7F43.jpeg"
+  },
+  {
+    name: "Golden Boss",
+    image: "591D21E0-1291-4CC4-AF01-61AACE883B3F.jpeg"
+  },
+  {
+    name: "Buddha Blossom",
+    image: "60A96B54-359F-4A5D-AC12-E4C7BB35F4AD.jpeg"
+  },
+  {
+    name: "Wild Cub Blossom",
+    image: "92848336-88AA-417F-B8C0-3E16C05FAFD0.jpeg"
+  },
+  {
+    name: "Festive Red",
+    image: "C0FF78B7-1240-458D-87B7-45C7893B95A3.jpeg"
+  },
+  {
+    name: "Midnight Amethyst",
+    image: "CB2B1DBC-C400-47E8-97F3-9D4BCC714A76.jpeg"
+  },
+  {
+    name: "Buddha Serenity",
+    image: "344A416B-E8E6-453A-8816-99B3118F75CB.jpeg"
+  },
+  {
+    name: "Safari Serenity",
+    image: "9671E222-1043-4E27-8D77-F4FE6220F22D.jpeg"
+  },
+  {
+    name: "Royal Bloom",
+    image: "Royal blom.jpeg"
+  },
+  {
+    name: "Pink Boss",
+    image: "1CA6DA21-8A41-44EE-B942-978C1B8B87C8.jpeg"
+  },
+  {
+    name: "Baddie Bloom",
+    image: "CD8CAEE4-0A88-4606-B41C-7F70D9B8E87B.jpeg"
+  },
+  {
+    name: "Sakura Dream",
+    image: "9FDC8FC3-6FAE-461D-A9DC-4A952768FE2D.jpeg"
+  },
+  {
+    name: "Safari Sunshine",
+    image: "CDAB04F2-5CD5-415C-9C03-6B504A0FC0E8.jpeg"
+  },
+  {
+    name: "Sweet Kiss",
+    image: "EA35D7F2-C79B-4510-8035-DC969A386A6F.jpeg"
+  },
+  {
+    name: "Steampunk Garden",
+    image: "A66F9AF2-4DB1-49A5-B26E-6275E1AE942B.jpeg"
+  },
+  {
+    name: "Orchid Fantasy",
+    image: "254DE367-D943-4F6D-8C5A-D249A07E79AF.jpeg"
+  },
+  {
+    name: "Buddha Butterfly Bliss",
+    image: "A9670A4F-A59C-4C07-818A-151EADBB1F40.jpeg"
+  },
+  {
+    name: "Pastel Hydrangea Dream",
+    image: "0AFC2D52-2A83-4966-A284-044CF300C075.jpeg"
+  },
+  {
+    name: "Golden Boho Muse",
+    image: "E48A4483-EA44-4FB7-BD81-184FFD39BE7A.jpeg"
+  },
+  {
+    name: "Sunset Rider",
+    image: "8138D381-B8C8-4078-A7EB-12696E7EEA2C.jpeg"
+  },
+  {
+    name: "Enchanted Forest",
+    image: "D4FF9A12-EF43-4731-A477-D8C78746EE87.jpeg"
+  },
+  {
+    name: "Guitar Romance",
+    image: "5C654EED-8BD8-4A59-A4C8-8C359C56CAF3.jpeg"
+  },
+  {
+    name: "Neon Owl",
+    image: "6ED4B700-88C1-4BC4-89A3-B079BF9F11D0.jpeg"
+  },
+  {
+    name: "Grappige zebra",
+    image: "8248FBF1-6237-47D3-9EC8-2B40C029766F.jpeg"
+  },
+  {
+    name: "Kleurrijke pauw",
+    image: "3D472D97-0866-4403-98E3-3BDCEA39314E.jpeg"
+  },
+  {
+    name: "Majestueuze leeuw",
+    image: "6A389EF0-BA6C-4E80-B213-FD2131D46919.jpeg"
+  },
+  {
+    name: "Gangster glamour",
+    image: "7921D363-C3F1-489A-8ADD-84253BDCDD85.jpeg"
+  },
+  {
+    name: "Elegante zebra",
+    image: "B476C5F4-D6BE-40CB-B7A1-534ED949C72A.jpeg"
+  },
+  {
+    name: "Elegante krokodil",
+    image: "A5A6B12B-909E-44DC-92F0-8407A3A0F2A1.jpeg"
+  },
+  {
+    name: "Butterfly Dreams",
+    image: "90152F7B-52F2-4CD5-BC50-2641C3888F91.jpeg"
+  },
+  {
+    name: "Tropical Paradise",
+    image: "9FA66EC2-C61C-44E9-97C7-033A707742AD.jpeg"
   }
 ];
 
 const beschikbaarGrid = document.getElementById("all-season-beschikbaar");
+const verkochtGrid = document.getElementById("all-season-verkocht");
 
 allSeasonVazen.forEach((vaas) => {
   const kaart = document.createElement("div");
@@ -21,4 +314,17 @@ allSeasonVazen.forEach((vaas) => {
   `;
 
   beschikbaarGrid.appendChild(kaart);
+});
+
+allSeasonVerkocht.forEach((vaas) => {
+  const kaart = document.createElement("div");
+  kaart.className = "fotoboek-item";
+
+  kaart.innerHTML = `
+    <img src="${vaas.image}" alt="${vaas.name} vaas">
+    <h2>${vaas.name}</h2>
+    <p>SOLD OUT</p>
+  `;
+
+  verkochtGrid.appendChild(kaart);
 });
