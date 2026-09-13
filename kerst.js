d668ee47214f83c303ff0d97afe05f4528ecf946
CraftedMemories <creatief-knutselen@outlook.com>
2026-09-13 14:02:52 +0200

    Update kerst.js

--- kerst.js
+++ kerst.js
@@ -1,5 +1,110 @@
 const kerstVazen = [
   {
+  name: "Cozy Christmas Santa",
+  price: "€12,50",
+  image: "E79E17FC-B794-4379-8AD0-D01E4B9F0FBD.png"
+},
+{
+  name: "Merry Gnome",
+  price: "€12,50",
+  image: "74EE5A4B-6A8E-47F3-8F11-C68FE34E71A7.png"
+},
+{
+  name: "Merry & Bright Santa",
+  price: "€12,50",
+  image: "3311D91A-D4B0-4F45-BE32-6031794F9FE3.png"
+},
+{
+  name: "Christmas Little Santa",
+  price: "€12,50",
+  image: "AF242E7D-35C8-4A14-94C0-4A40DFE00F78.png"
+},
+{
+  name: "Christmas Friends",
+  price: "€12,50",
+  image: "7DFB2C8A-BF26-4C67-8171-D68210141642.png"
+},
+{
+  name: "Santa’s Christmas Joy",
+  price: "€12,50",
+  image: "C961AA72-DC2E-4F27-9662-2645C75F08B2.png"
+},
+{
+  name: "Christmas Deer",
+  price: "€12,50",
+  image: "15C2CDF2-A278-410C-BEA4-5097F6A42AC2.png"
+},
+{
+  name: "Christmas Teddy",
+  price: "€12,50",
+  image: "7BF5DD2B-4BF1-43C9-A609-E0358AC68D86.png"
+},
+{
+  name: "Santa’s Winter Magic",
+  price: "€12,50",
+  image: "74B39217-CF60-4665-88E0-FBA2B2B9E601.png"
+},
+{
+  name: "Leopard Christmas",
+  price: "€12,50",
+  image: "41C79D69-10C0-4AB7-BD0B-A128C1484F4E.png"
+},
+{
+  name: "Blue Christmas Santa",
+  price: "€12,50",
+  image: "9A21F96A-6878-43F4-8A6F-0B06BCD47E0B.png"
+},
+{
+  name: "Pink Christmas Friends",
+  price: "€12,50",
+  image: "24E387A3-651B-4BDD-98A6-D6758B2C54C2.png"
+},
+{
+  name: "Christmas Gnome",
+  price: "€12,50",
+  image: "E4CFE4A7-D635-4251-9056-4A6E730AF8A3.png"
+},
+{
+  name: "Pink Santa & Snowman",
+  price: "€12,50",
+  image: "19BE5FAD-3A71-46E7-BF72-19AE839C7635.png"
+},
+{
+  name: "Winking Santa",
+  price: "€12,50",
+  image: "EE46043D-67AE-4751-B519-FD32E830A54C.png"
+},
+{
+  name: "Santa’s Christmas Train",
+  price: "€12,50",
+  image: "188EDFF9-5D8B-4892-9F63-44CD4DF310A6.png"
+},
+{
+  name: "Santa’s Sleigh Ride",
+  price: "€12,50",
+  image: "151A74B3-2B3D-456A-BAF0-8511ABD46DF0.png"
+},
+{
+  name: "Green Christmas Friends",
+  price: "€12,50",
+  image: "7D91A68B-87FD-4452-9D4B-ED4C85A38064.png"
+},
+{
+  name: "Gingerbread Christmas",
+  price: "€12,50",
+  image: "84CF9365-A593-40E7-9AEB-F4C2AFA0A76E.png"
+},
+{
+  name: "Cozy Christmas Reindeer",
+  price: "€12,50",
+  image: "FE7B7F1E-3036-4FF3-9D3D-C4BB6972CAB1.png"
+},
+{
+  name: "Mickey & Minnie Christmas",
+  price: "€12,50",
+  image: "5F478FEE-031E-4EB3-B7A3-3A7C2C9D2D47.jpeg"
+},
+  {
     name: "Winter Magic",
     image: "58DCDA64-042F-4ABE-B4F7-5D0205488E88.jpeg",
     price: "€20,00"
